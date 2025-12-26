e6d068d66f9e9130750aa0fb47b2815d9afafee7
Walker Davis <walkerreeddavis@gmail.com>
2025-12-26 10:35:59 -0600

    Merry Christmas

--- starter_code/HolidayArt.java
+++ starter_code/HolidayArt.java
@@ -15,6 +15,9 @@ public class HolidayArt {
     public static void main(String[] args) {
         // TODO: Parse command-line argument for tree height (default: 5)
         int height = 5;
+        if(args.length > 0){
+            height = Integer.parseInt(args[0]);
+        }
 
         // TODO: Print the tree
         printTree(height);
@@ -27,14 +30,11 @@ public class HolidayArt {
      */
     public static void printTree(int height) {
         // TODO: Implement this method
-        //
-        // Steps:
-        // 1. Print the star on top (centered)
-        // 2. Loop through each level of branches
-        // - Calculate spaces needed for centering
-        // - Calculate stars needed (1, 3, 5, 7, ...)
-        // 3. Print the trunk (centered)
-
-        System.out.println("Implement me!");
+        for(int i =0; i < height; i++){
+            int stars = (i*2)+1;
+            int spaces = height-i-1;
+            System.out.println(" ".repeat(spaces)+"*".repeat(stars)+" ".repeat(spaces));
+        }
+        System.out.println(" ".repeat(height-2)+"|||");
     }
 }
